a0a29ebf2f5f896b3e5a4c73802c4e672d367608
Niko Gulan <niko.gulan@outlook.com>
2026-07-08 23:24:15 +0200

    Refactor fuel.py for improved readability and logic

--- CS50's Introduction to Python/3. Exceptions/fuel.py
+++ CS50's Introduction to Python/3. Exceptions/fuel.py
@@ -1,28 +1,23 @@
-def main():
-
-    while True:
-        try:
-            fraction = input("Fraction: ")
-            splitted = fraction.split("/")
-            x = int(splitted[0])
-            y = int(splitted[1])
-            if x > y:
-                continue
-            elif x < 0 or y <= 0:
-                raise ValueError
-            else:
-                result = int((x / y) * 100)
-        except (ValueError, ZeroDivisionError):
-            pass
+while True:
+    try:
+        fraction = input("Fraction: ")
+        x, y = fraction.split("/")
+        x = int(x)
+        y = int(y)
+        if x > y:
+            continue
+        elif x < 0 or y <= 0:
+            raise ValueError
         else:
-            break
-
-    if rounded_result >= 99:
-        print("F")
-    elif rounded_result <= 1:
-        print("E")
+            result = round((x / y) * 100)
+    except (ValueError, ZeroDivisionError):
+        pass
     else:
-        print(f"{rounded_result}%")
+        break
 
-
-main()
+if result >= 99:
+    print("F")
+elif result <= 1:
+    print("E")
+else:
+    print(f"{result}%")
